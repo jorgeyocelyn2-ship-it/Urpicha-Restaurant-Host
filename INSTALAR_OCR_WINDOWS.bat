10365d05bdcd118f81bce345800c15b800c9782c
jorgeyocelyn2-ship-it <jorgeyocelyn2@gmail.com>
2026-08-07 22:16:31 -0500

    Delete INSTALAR_OCR_WINDOWS.bat

--- INSTALAR_OCR_WINDOWS.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Se abrira la pagina del instalador de Tesseract para Windows.
-echo Durante la instalacion, incluye el idioma Spanish / spa si aparece como opcion.
-start "" "https://github.com/UB-Mannheim/tesseract/wiki"
-pause
